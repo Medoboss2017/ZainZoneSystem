29b652fe698edc2cd28876c3729b0021182f3e14
Medoboss2017 <44714458+Medoboss2017@users.noreply.github.com>
2019-01-29 18:54:22 +0200

    Create System.js

---
+++ System.js
@@ -0,0 +1,316 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`I'm Ready ${client.user.tag}!`);
+});
+
+client.on('message', function(msg) {
+  var prefix = "!!";	
+if(msg.content.startsWith (prefix  + 'serverinfo')) {
+ let embed = new Discord.RichEmbed()
+ .setColor('RANDOM')
+ .setThumbnail(msg.guild.iconURL)
+ .setTitle(`Showing Details Of  **${msg.guild.name}*`)
+ .addField('🌐** نوع السيرفر**',`[** __${msg.guild.region}__ **]`,true)
+ .addField('🏅** __الرتب__**',`[** __${msg.guild.roles.size}__ **]`,true)
+ .addField('🔴**__ عدد الاعضاء__**',`[** __${msg.guild.memberCount}__ **]`,true)
+ .addField('🔵**__ عدد الاعضاء الاونلاين__**',`[** __${msg.guild.members.filter(m=>m.presence.status == 'online').size}__ **]`,true)
+ .addField('📝**__ الرومات الكتابية__**',`[** __${msg.guild.channels.filter(m => m.type === 'text').size}__** ]`,true)
+ .addField('🎤**__ رومات الصوت__**',`[** __${msg.guild.channels.filter(m => m.type === 'voice').size}__ **]`,true)
+ .addField('👑**__ الأونـر__**',`**${msg.guild.owner}**`,true)
+ .addField('🆔**__ ايدي السيرفر__**',`**${msg.guild.id}**`,true)
+ .addField('📅**__ تم عمل السيرفر في__**',msg.guild.createdAt.toLocaleString())
+ msg.channel.send({embed:embed});
+}
+});
+
+
+ client.on('message', message => {
+ 
+ var ms = require('ms')
+   
+ var prefix = "!!";
+
+ var moment = require('moment');
+ 
+ if (message.author.bot) return;
+ 
+ if (!message.content.startsWith(prefix)) return;
+ 
+ let command = message.content.split(" ")[0];
+ 
+ command = command.slice(prefix.length);
+ 
+ let args = message.content.split(" ").slice(1);
+ 
+ let messageArray = message.content.split(" ");
+ 
+ let embed = new Discord.RichEmbed()
+ 
+.setImage("https://d.top4top.net/p_10880lo1r1.png")
+ 
+ if (command == "ban") {
+ 
+ if(!message.channel.guild) return message.reply('** This command only for servers**');
+         
+ if(!message.guild.member(message.author).hasPermission("BAN_MEMBERS")) return message.reply("**:x: You Don't Have ` BAN_MEMBERS ` Permission**");
+ 
+ if(!message.guild.member(client.user).hasPermission("BAN_MEMBERS")) return message.reply("**:x: I Don't Have ` BAN_MEMBERS ` Permission**");
+ 
+ let user = message.mentions.users.first();
+ 
+ let Reason = message.content.split(" ").slice(3).join(" ");
+ 
+ let time = messageArray[2];
+ 
+ if (message.mentions.users.size < 1) return message.channel.sendEmbed(embed)
+ 
+ if (!message.guild.member(user).bannable) return message.reply("**:x:I Don't Have Permission For Ban This User**");
+ 
+ if(!time.match(/[1-60][s,m,h,d,w]/g))  return message.channel.send(':x: This Time Is Incorrect')
+ 
+ if(!Reason)  {
+ 
+ message.guild.member(user).ban({reason: Reason})
+ 
+ }
+ 
+  if(!Reason && time) {
+ 
+  message.guild.member(user).ban(7, user);
+ 
+  }  
+ 
+  if(!time) {
+ 
+  message.guild.member(user).ban(7, user);
+ 
+  }
+  if(time) {
+ 
+  setTimeout(() => {
+ 
+  message.guild.unban(user);
+ 
+  }, ms(time));
+ 
+  }
+ 
+  if(time && Reason && user) {
+ 
+  message.guild.member(user).ban({reason: Reason})
+     
+     
+  setTimeout(() => {
+ 
+  message.guild.unban(user);
+ 
+  }, ms(time));
+ 
+  }
+ 
+  message.channel.send(`:white_check_mark:  ${user.tag} banned from the server ! :airplane:`)
+ 
+  }
+ 
+  });
+ 
+
+client.on('message', message => {
+ 
+  var ms = require('ms')
+ 
+  var prefix = "!!";
+
+  var moment = require('moment');
+ 
+   
+  if (message.author.bot) return;
+ 
+  if (!message.content.startsWith(prefix)) return;
+ 
+  let command = message.content.split(" ")[0];
+ 
+  command = command.slice(prefix.length);
+ 
+  let args = message.content.split(" ").slice(1);
+ 
+  let messageArray = message.content.split(" ");
+ 
+  let muteRole = message.guild.roles.find("name", "Muted");
+   
+  let embed = new Discord.RichEmbed()
+ 
+ .setImage("https://c.top4top.net/p_108825enp1.png")
+ 
+  if (command == "mute") {
+   
+  if(!muteRole) return message.guild.createRole({ name: "Muted", permissions: [] });
+ 
+  if(!message.channel.guild) return message.reply('** This command only for servers**');
+         
+  if(!message.guild.member(message.author).hasPermission("MUTE_MEMBERS")) return message.reply("**:x: You Don't Have ` MUTE_MEMBERS ` Permission**");
+ 
+  if(!message.guild.member(client.user).hasPermission("MUTE_MEMBERS")) return message.reply("**:x: I Don't Have ` MUTE_MEMBERS ` Permission**");
+ 
+  let user = message.mentions.users.first();
+ 
+  let Reason = message.content.split(" ").slice(4).join(" ");
+ 
+  let time = messageArray[2];
+ 
+  if (message.mentions.users.size < 1) return message.channel.sendEmbed(embed)
+   
+  if (!message.guild.member(user).bannable) return message.reply("**:x:I Don't Have Permission For Mute This User**");
+ 
+  if(!Reason)  {
+ 
+    message.guild.member(user).addRole(muteRole);
+ 
+  }
+ 
+   if(!Reason && time) {
+ 
+    message.guild.member(user).addRole(muteRole);
+ 
+   }  
+ 
+   if(!time) {
+ 
+    message.guild.member(user).addRole(muteRole);
+ 
+   }
+   if(time) {
+    if(!time.match(/[1-60][s,m,h,d,w]/g))  return message.channel.send(':x: This Time Is Incorrect')
+ 
+   setTimeout(() => {
+ 
+    message.guild.member(user).removeRole(muteRole);
+ 
+   }, ms(time));
+ 
+   }
+ 
+   if(time && Reason && user) {
+ 
+    message.guild.member(user).addRole(muteRole);
+ 
+       
+   setTimeout(() => {
+ 
+    message.guild.member(user).removeRole(muteRole);
+   
+   }, ms(time));
+ 
+   }
+ 
+   message.channel.send(`:white_check_mark: ${user} has been muted ! :zipper_mouth:`)
+ 
+   }
+ 
+});
+
+client.on('guildMemberAdd', member => {
+
+    const channel = member.guild.channels.find('name', 'welcome');
+  
+    const millis = new Date().getTime() - member.user.createdAt.getTime();
+    const now = new Date();
+    const createdAt = millis / 1000 / 60 / 60 / 24;
+
+
+
+
+  
+    const embed = new Discord.RichEmbed()
+    
+    .setColor("black")
+    .setDescription(`**تاريخ دخولك للدسكورد منذ ${createdAt.toFixed(0)} يوم**`)
+    .setAuthor(member.user.tag, member.user.avatarURL);
+    channel.sendEmbed(embed);
+
+  
+});
+
+
+client.on('message', async msg => {
+  var prefix = "!!";
+  let message = msg;
+  if(msg.content.startsWith(prefix + "bans")){
+    let bans = await msg.guild.fetchBans();
+    let array = [];
+    await bans.forEach(async user=>{
+      array.push(user.id);
+    });
+    let page = 0;
+    let pages = parseInt(`${array.length}`.slice(0,-1),10);
+    const topembed = new Discord.RichEmbed()
+      .setAuthor(`Banned Users`)
+      .addField(`الشخص`, `<@!${array[0]}>\n<@!${array[1]}>\n<@!${array[2]}>\n<@!${array[3]}>\n<@!${array[4]}>\n<@!${array[5]}>\n<@!${array[6]}>\n<@!${array[7]}>\n<@!${array[8]}>\n<@!${array[9]}>\n<@!${array[10]}>`, true)
+      .setColor('RANDOM')
+      .setFooter(`Page ${page+1} of ${pages}`)
+      .setTimestamp();
+    message.channel.send(topembed).then(async topmessage=>{
+      topmessage.react(`◀`).then(()=>{
+        topmessage.react(`▶`).then(()=>{
+          const backwardsFilter = (reaction, user) => reaction.emoji.name === '◀' && user.id === message.author.id;
+          const forwardsFilter = (reaction, user) => reaction.emoji.name === '▶' && user.id === message.author.id;
+          const backwards = topmessage.createReactionCollector(backwardsFilter, {time: 120000});
+          const forwards = topmessage.createReactionCollector(forwardsFilter, {time: 120000});
+          backwards.on("collect", r=>{
+            r.remove(message.author);
+            if(page <= 0) return;
+            page--;
+            let newembed = new Discord.RichEmbed()
+              .setAuthor(`Banned Users`)
+              .addField(`الشخص`, `<@!${array[0]}>\n<@!${array[1]}>\n<@!${array[2]}>\n<@!${array[3]}>\n<@!${array[4]}>\n<@!${array[5]}>\n<@!${array[6]}>\n<@!${array[7]}>\n<@!${array[8]}>\n<@!${array[9]}>\n<@!${array[10]}>`, true)
+              .setColor("RANDOM")
+              .setFooter(`Page ${page+1} of ${pages}`)
+              .setTimestamp()
+            topmessage.edit(newembed);
+          })
+          forwards.on("collect", r=>{
+            r.remove(message.author);
+            if(page === pages) return;
+            page++;
+            let newembed = new Discord.RichEmbed()
+              .setAuthor(`Banned Users`)
+              .addField(`الشخص`, `<@!${array[0]}>\n<@!${array[1]}>\n<@!${array[2]}>\n<@!${array[3]}>\n<@!${array[4]}>\n<@!${array[5]}>\n<@!${array[6]}>\n<@!${array[7]}>\n<@!${array[8]}>\n<@!${array[9]}>\n<@!${array[10]}>`, true)
+              .setColor("RANDOM")
+              .setFooter(`Page ${page+1} of ${pages}`)
+              .setTimestamp()
+            topmessage.edit(newembed);
+          });
+        });
+      });
+    });
+  };
+});
+
+client.on('ready',async () => {
+setInterval(function(){
+var currentTime = new Date(),
+hours = currentTime.getHours() + 3 ,
+ReBeeL = currentTime.getMinutes(),
+ReBeeeL = currentTime.getSeconds(),
+Codes = currentTime.getFullYear(),
+CodeS = currentTime.getMonth() + 1,
+CoDeS = currentTime.getDate()
+if (ReBeeL < 10) {
+ReBeeL = "0" + ReBeeL;
+}
+var suffix = "AM";
+if (hours >= 12) {
+suffix = "PM";
+hours = hours - 12;
+}
+if (hours == 0) {
+hours = 12;
+}
+client.channels.find('id', '539825183977111553').setName(`Time - ${hours} : ${ReBeeL} : ${ReBeeeL} ${suffix}`) 
+client.channels.find('id', '539825238263726093').setName(`Date : ${Codes} - ${CodeS} - ${CoDeS}`)
+}, 1000);
+});
+
+client.login(process.env.BOT_TOKEN);
